2f5b3e60599d95f99e2c93a451a051a37d01c0b8
Fred Chien <cfsghost@gmail.com>
2013-11-24 21:44:06 +0800

    loading configuration files before configuring frex.js

--- app.js
+++ app.js
@@ -6,30 +6,32 @@ var ConfigManager = require('./lib/config_manager');
 
 var app = Frex();
 
-app.configure(function() {
-
-	app.set('views', __dirname + '/views');
-	app.set('view engine', 'jade');
-	app.use(function(req, res, next) {
-		res.locals.configs = app.locals.configs;
-		next();
-	});
-
-	// Session
-	app.use(Frex.cookieParser());
-	app.use(Frex.cookieSession({
-		key: 'fulfill',
-		secret: app.locals.configs.app.secret_key
-	}));
-
-	app.use(app.router);
-	app.use(Frex.static(__dirname + '/public'));
-});
-
 // Loading configuration files
 var configManager = new ConfigManager(path.join(__dirname, 'configs'));
 configManager.load(function(err, configs) {
+
 	app.locals.configs = configs;
+
+	// Configuring
+	app.configure(function() {
+
+		app.set('views', __dirname + '/views');
+		app.set('view engine', 'jade');
+		app.use(function(req, res, next) {
+			res.locals.configs = app.locals.configs;
+			next();
+		});
+
+		// Session
+		app.use(Frex.cookieParser());
+		app.use(Frex.cookieSession({
+			key: 'fulfill',
+			secret: app.locals.configs.app.secret_key
+		}));
+
+		app.use(app.router);
+		app.use(Frex.static(__dirname + '/public'));
+	});
 
 	app.listen(app.locals.configs.app.port, function() {
 		console.log('website is ready.');
