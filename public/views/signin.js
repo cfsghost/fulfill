10aa61e33116cf2557bc518cd84c7317b64df6bc
Fred Chien <cfsghost@gmail.com>
2013-11-29 16:13:41 +0800

    check input boxes

--- public/views/signin.js
+++ public/views/signin.js
@@ -4,9 +4,17 @@ App.require('User', function() {
 
 	$('.popup').popup();
 
+	// Focus on username input box by default
+	$('#signin_username').focus();
+
 	$('#signin_submit_btn').on('click', function() {
 
 		if ($('#signin_username').val() == '' || $('#signin_password').val() == '') {
+
+			$('#signin_field_username').addClass('error');
+			$('#signin_field_password').addClass('error');
+
+			$('#signin_username').focus();
 			return;
 		}
 	});
