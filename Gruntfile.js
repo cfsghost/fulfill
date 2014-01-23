6859c67c2dff0313eb60b7981a2b4519a0359b9c
Fred Chien <cfsghost@gmail.com>
2014-01-24 00:45:07 +0800

    usgin grunt to build project

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+var FrontendScript = require('./dev/frontend_script');
+
+module.exports = function(grunt) {
+
+	// Front-end scripts
+	var frontendScript = new FrontendScript(grunt);
+	frontendScript.viewScripts = {
+
+		// Javascript for homepage
+		'public/views/index.js': [ 'frontend/views/index.js' ],
+
+		// JavaScript file for user pages
+		'public/views/user/forget.js': [ 'frontend/views/user/forget.js' ],
+		'public/views/user/profile.js': [ 'frontend/views/user/profile.js' ],
+		'public/views/user/reset_password.js': [ 'frontend/views/user/reset_password.js' ],
+		'public/views/user/signin.js': [ 'frontend/views/user/signin.js' ],
+		'public/views/user/signup.js': [ 'frontend/views/user/signup.js' ],
+
+		// JavaScript files for admin Pages
+		'public/views/admin/index.js': [ 'frontend/views/admin/index.js' ]
+	};
+	frontendScript.updateSources();
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			view: {
+				files: frontendScript.gruntSettings
+			}
+		},
+		watch: {
+			scripts: {
+				files: frontendScript.watchScripts,
+				tasks: 'uglify:view'
+			},
+			options: {
+				nospawn: true
+			}
+		}
+	});
+
+	grunt.event.on('watch', function(action, filepath) {
+
+		frontendScript.watch(action, filepath);
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('build', [
+		'uglify:view'
+	]);
+};
---
+++ dev/frontend_script.js
@@ -0,0 +1,104 @@
+"use strict";
+
+var fs = require('fs');
+var path = require('path');
+
+var FrontendScript = module.exports = function(grunt) {
+	var self = this;
+
+	self.grunt = grunt;
+	self.gruntSettings = {};
+	self.sourceDir = 'frontend';
+	self.destDir = 'public';
+	self.viewScripts = {};
+	self.watchScripts = [];
+};
+
+FrontendScript.prototype.updateSources = function() {
+	var self = this;
+
+	for (var filepath in self.viewScripts) {
+		var sources = self.prepareSources(self.viewScripts[filepath]);
+
+		self.gruntSettings[filepath] = sources;
+	}
+};
+
+FrontendScript.prototype.prepareSources = function(srcList) {
+	var self = this;
+
+	var sources = [];
+	for (var index in srcList) {
+		var filepath = srcList[index];
+
+		if (fs.existsSync(filepath)) {
+			// Try to scan files in the directory.
+			if (fs.lstatSync(filepath).isDirectory()) {
+				sources = sources.concat(self.scanFiles(filepath));
+
+				self.watchScripts.push(path.join(filepath, '*.js'));
+			} else {
+				// It is source code file, add to list
+				sources.push(filepath);
+
+				self.watchScripts.push(filepath);
+			}
+		}
+	}
+
+	return sources;
+};
+
+FrontendScript.prototype.scanFiles = function(filepath) {
+	var self = this;
+
+	var subFiles = [];
+	var curFiles = [];
+	var files = fs.readdirSync(filepath);
+	for (var filename in files) {
+
+		var realPath = path.join(filepath, filename);
+
+		// It is directory, just scanning it.
+		if (fs.lstatSync(realPath).isDirectory()) {
+			subFiles = subFiles.concat(self.scanFiles(realPath));
+
+			self.watchScripts.push(path.join(realPath, '*.js'));
+		} else {
+			curFiles.push(realPath);
+		}
+	}
+
+	return curFiles.concat(subFiles);
+}
+
+FrontendScript.prototype.watch = function(action, filepath) {
+	var self = this;
+
+	var levelSets = filepath.split(path.sep);
+	levelSets[0] = self.destDir;
+
+	// Finding which group this file belongs to
+	var dest = null;
+	var src = null;
+	for (var destPath in self.viewScripts) {
+		if (self.viewScripts[destPath].indexOf(filepath) != -1 ) {
+			dest = destPath;
+			src = self.viewScripts[destPath];
+			break;
+		}
+	}
+
+	if (dest && src) {
+
+		// Update the config file to only build the changed file.
+		self.grunt.config('uglify.view.files', [
+			{
+				src: src,
+				dest: dest
+				//src: filepath,
+				//dest: levelSets.join(path.sep)
+			}
+		]);
+	}
+};
